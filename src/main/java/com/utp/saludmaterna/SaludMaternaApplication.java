792ebdd70053230c10b4af6ba3eec93ae30336a1
Leonardo Espejo <85245534+Leo-Spj@users.noreply.github.com>
2024-09-26 22:00:04 -0500

    FC

---
+++ src/main/java/com/utp/saludmaterna/SaludMaternaApplication.java
@@ -0,0 +1,13 @@
+package com.utp.saludmaterna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaludMaternaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaludMaternaApplication.class, args);
+    }
+
+}
